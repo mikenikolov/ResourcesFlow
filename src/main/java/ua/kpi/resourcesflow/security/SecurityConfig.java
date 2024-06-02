1a52d101ddc351e3a794d457556fba02e2f972bf
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2024-06-02 15:29:33 +0300

    feat: add roles & denied permission popup

--- src/main/java/ua/kpi/resourcesflow/security/SecurityConfig.java
+++ src/main/java/ua/kpi/resourcesflow/security/SecurityConfig.java
@@ -2,10 +2,13 @@ package ua.kpi.resourcesflow.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,6 +22,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/login", "/css/**").permitAll()
+                                .requestMatchers("machines/*/add-expenses", "machines/add", "/types")
+                                .hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
@@ -38,10 +43,14 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        manager.createUser(User.withUsername("admin")
+                .password(encoder.encode("pass"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("user")
+                .password(encoder.encode("pass"))
                 .roles("USER")
                 .build());
         return manager;
